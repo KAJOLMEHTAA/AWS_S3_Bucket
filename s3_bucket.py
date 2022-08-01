2960b3979d5642b91f81f8e7571b821d1cc575e7
KajolMehta <59251221+KAJOLMEHTAA@users.noreply.github.com>
2022-08-01 17:00:36 +0530

    Create s3_bucket.py

---
+++ s3_bucket.py
@@ -0,0 +1,38 @@
+import boto3
+import json
+from botocore.exceptions import ClientError
+import os
+
+def lambda_handler(event,context):
+    total_size = 0
+    Buckets = []
+    s3 = boto3.client('s3')
+    response = s3.list_buckets()
+
+    for bucket in response['Buckets']:
+        Buckets.append(bucket["Name"])
+
+    #Buckets = ["advice-column-media","ai.bitcotapps.com","amplify-myamplifyapp-dev-125809-deployment","asec-wordpress","mayday-assets.bitcotapps.com","bitcot-old-db-backup","asec-mediabucket","marketplace.bitcotapps.com","memory.net-staging","robotics-wordpress","bitcotassets","ims-ecomm-demo","outside-allfiles"]
+    count_dict = {}
+    for name in Buckets:
+        buckets = boto3.resource('s3').Bucket(name)
+        for object in buckets.objects.all():
+            total_size += object.size
+            #print(object.size)
+        count_dict[name] = total_size/1024/1024
+        value = dict(sorted(count_dict.items(), key=lambda x:x[1]))
+        total_size=0
+    #print(value)
+    for i,j in value.items():
+        print(i,":",j, "MB")
+        #print(name, ":", (total_size/ 1024 / 1024), "MB")
+    #total_size=0
+    c = dict((k, v) for k, v in value.items() if v >= 1024)
+    print(c)
+    #default_region = os.environ['AWS_REGION']
+    sns = boto3.client("sns", region_name="us-east-1")
+    sns.publish(
+          TopicArn="arn:aws:sns:us-east-1:686878367233:BucketList",
+          Message=json.dumps(c), 
+          Subject="Bucket List above 1GB"
+        )
